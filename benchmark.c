247907e15bb2febf8f9877b2684c600f81020637
Eyal Lotem <eyal.lotem@gmail.com>
2013-02-03 15:28:45 +0200

    Get element count from command line

--- benchmark.c
+++ benchmark.c
@@ -2,7 +2,6 @@
 #include <stdio.h>              /* printf */
 #include <stdlib.h>             /* malloc, abort */
 
-#define PAIR_COUNT (10000000)
 #define HASH_SIZE (1ULL<<16)
 
 struct pair {
@@ -16,15 +15,21 @@ static bool prefix__match(void *user_arg, const void *key, small_hash__node *nod
     return pair_of(node)->key == (uintptr_t)key;
 }
 
-int main() {
+static void usage(char *progname) { fprintf(stderr, "Usage: %s <count>\n", progname); }
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) { usage(argv[0]); return -1; }
+    long pair_count = atol(argv[1]);
+    if(pair_count <= 0) { usage(argv[0]); return -1; }
+
     small_hash__anchor *anchors = malloc(HASH_SIZE * sizeof *anchors);
     small_hash__table table;
     struct small_hash__funcs funcs = SMALL_HASH__FUNCS(prefix__);
     SMALL_HASH__TABLE__INIT_STATIC(&table, &funcs, NULL, anchors);
 
-    struct pair *pairs = malloc(PAIR_COUNT * sizeof *pairs);
+    struct pair *pairs = malloc(pair_count * sizeof *pairs);
     unsigned i;
-    for(i = 0; i < PAIR_COUNT; i++) {
+    for(i = 0; i < pair_count; i++) {
         pairs[i] = (struct pair){ SMALL_HASH__NODE__EMPTY, i, i };
         small_hash__table__add(&table, i, &pairs[i].node);
     }
