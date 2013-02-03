3e14f533840be03e5b777782e3a7964371dd8a4c
Eyal Lotem <eyal.lotem@gmail.com>
2013-02-03 16:09:23 +0200

    Extract small utils to its own header

--- small_hash.h
+++ small_hash.h
@@ -6,21 +6,15 @@
 #include <stdbool.h>
 
 typedef struct small_hash__table small_hash__table;
-typedef struct small_hash__node small_hash__node;
-typedef struct small_hash__anchor small_hash__anchor;
-typedef uint32_t small_hash__hash;
 
+typedef struct small_hash__node small_hash__node;
 #define SMALL_HASH__NODE__EMPTY  SMALL_HASH__NODE__EMPTY_INTERNAL
 
-#ifndef container_of
-/* TODO: Add a type-equals-compile-time-assertion on memberptr? */
-#define container_of(memberptr, containername, membername)  \
-    ((containername *)((char*)(memberptr) - offsetof(containername, membername)))
-#endif
+typedef struct small_hash__anchor small_hash__anchor;
 
-#ifndef ARRAY_LEN
-#define ARRAY_LEN(arr)    (sizeof (arr) / sizeof *(arr))
-#endif
+typedef uint32_t small_hash__hash;
+
+#include "utils.h"
 
 struct small_hash__funcs {
     bool (*match_key)(void *user_arg, const void *key, small_hash__node *);
---
+++ utils.h
@@ -0,0 +1,14 @@
+#ifndef __UTILS_H_
+#define __UTILS_H_
+
+#ifndef container_of
+/* TODO: Add a type-equals-compile-time-assertion on memberptr? */
+#define container_of(memberptr, containername, membername)  \
+    ((containername *)((char*)(memberptr) - offsetof(containername, membername)))
+#endif
+
+#ifndef ARRAY_LEN
+#define ARRAY_LEN(arr)    (sizeof (arr) / sizeof *(arr))
+#endif
+
+#endif
